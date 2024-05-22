8376573cd9334a8aa91668aa3c576bd6e8172289
Igor Jardim Bahia <L01942@lactec.com.br>
2024-05-22 16:48:03 -0300

    :sparkles: feat: Added new stages of studies.

---
+++ 19ModifadorDeParametrosRefEOut/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //ref faz o parâmetro ser uma referência para a variável original.
+            //Sem o ref imprimiria 10 e não 30.
+            //também exige que a palavra ref seja iniciada. Ou seja, não aceita se for int a;
+            //deve ser evitado
+            int a = 10;
+            Calculator.Triple(ref a);
+            Console.WriteLine(a);
+        }
+    }
+}
---
+++ 20ModificadorDeParametrosOut/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //ref faz o parâmetro ser uma referência para a variável original.
+            //Sem o ref imprimiria 10 e não 30.
+            //deve ser evitado
+            int a = 10;
+            int triple;
+            Calculator.Triple(a, out triple);
+            Console.WriteLine(triple);
+        }
+    }
+}
