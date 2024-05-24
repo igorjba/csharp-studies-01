cb934fbcc0c21b3b8cf42ce328eec2303b3aba58
Igor Jardim Bahia <L01942@lactec.com.br>
2024-05-24 15:14:40 -0300

    :sparkles: feat: Another day of studies.

--- Projeto24Matrizes/Program.cs
+++ Projeto24Matrizes/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace Projeto24Matrizes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double[,] matriz = new double[2, 3];
+
+            //Quantos elementos a matriz tem no total
+            Console.WriteLine(matriz.Length); //6
+
+            //Quanto é a primeira dimensão da matriz (quantidade de linhas). 
+            Console.WriteLine(matriz.Rank); //2
+
+            //Tamanho da dimensão especificada (no caso zero é a primeira dimensão - linhas)
+            Console.WriteLine(matriz.GetLength(0)); //2
+
+            //Tamanho da dimensão especificada (no caso um é a segunda dimensão - colunas)
+            Console.WriteLine(matriz.GetLength(1)); //3
+        }
+    }
+}
---
+++ Projeto25Exercicio08/Program.cs
@@ -0,0 +1,47 @@
+﻿using System.Threading.Channels;
+
+namespace Projeto25Exercicio08
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            int[,] mat = new int[n, n];
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] values = Console.ReadLine().Split(' ');
+
+                for (int j = 0; j < n; j++)
+                {
+                    mat[i,j] = int.Parse(values[j]);
+                }
+            }
+            Console.WriteLine();
+            Console.WriteLine("Main diagonal:");
+
+
+            int count = 0;
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write(mat[i,i] + " ");
+            }
+                Console.WriteLine();
+                Console.WriteLine();
+
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    if (mat[i,j] < 0)
+                    {
+                        count++;
+                    }
+                }
+            }
+            Console.WriteLine($"Negative numbers: {count}");
+        }
+    }
+}
